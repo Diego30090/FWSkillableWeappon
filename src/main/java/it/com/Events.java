0ebd210e5cf88ce1b76db5d24e8bc013ebb74442
Diego30090 <diego.vaccarini@gmail.com>
2021-03-23 14:51:46 +0100

    Added the randomized item drop (a standard drop) per creeper

--- src/main/java/it/com/Events.java
+++ src/main/java/it/com/Events.java
@@ -2,27 +2,40 @@ package it.com;
 
 
 
+import it.com.generalFunctions.Randomizer;
 import org.bukkit.Material;
 import org.bukkit.entity.*;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
 
 public class Events implements Listener {
+    Randomizer random =new Randomizer();
+    Material[] Weapons= new Material[]{Material.NETHERITE_AXE, Material.NETHERITE_SWORD, Material.CROSSBOW,Material.BOW};
+
 
     @EventHandler
     public void onMobDeath(EntityDeathEvent event){
-    event.getDrops().clear();
+        double MobPercentage= random.Random();
+        double WeaponPercentage=random.Random();
+        int probability=90; //in seguito da mettere da config
         LivingEntity e= event.getEntity();
 
-        if(e instanceof Creeper)
-            e.getLocation().getWorld().dropItem(e.getLocation(), new ItemStack(Material.NETHERITE_AXE));
+        double WeaponPercentage2=100/Weapons.length;
+        double EffectivePercentage = random.Random();
+
+        int weaponSelect=(int)(EffectivePercentage/WeaponPercentage2);
+
+        if(e instanceof Creeper) {
+            if (MobPercentage < probability)
+                e.getLocation().getWorld().dropItem(e.getLocation(), new ItemStack(Weapons[weaponSelect]));
         }
     }
+
+
+}
 
 
 
